2932ccd9df949541d147fae6270513dbde4b8584
User-CruZer <ahmadzakialbadri@gmail.com>
2026-04-15 12:01:56 +0700

    testing environment

--- resources/views/v_layouts/app.blade.php
+++ resources/views/v_layouts/app.blade.php
@@ -398,6 +398,25 @@
         <!-- /container -->
     </footer>
     <!-- /FOOTER -->
+    <!-- Account -->
+    <li class="header-account dropdown default-dropdown">
+        <div class="dropdown-toggle" role="button" data-toggle="dropdown" aria-expanded="true">
+            <div class="header-btns-icon">
+                <i class="fa fa-user-o"></i>
+            </div>
+            <strong class="text-uppercase">Akun Saya<i class="fa fa-caret-down"></i></strong>
+        </div>
+        <a href="#" class="text-uppercase">Login</a>
+        <ul class="custom-menu">
+            <li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
+            <li><a href="#"><i class="fa fa-heart-o"></i> My Wishlist</a></li>
+            <li><a href="#"><i class="fa fa-exchange"></i> Compare</a></li>
+            <li><a href="#"><i class="fa fa-check"></i> Checkout</a></li>
+            <li><a href="#"><i class="fa fa-unlock-alt"></i> Login</a></li>
+            <li><a href="#"><i class="fa fa-user-plus"></i> Create An Account</a></li>
+        </ul>
+    </li>
+    <!-- /Account -->
 
     <!-- jQuery Plugins -->
     <script src="{{ asset('frontend/js/jquery.min.js') }}"></script>
